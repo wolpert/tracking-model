569ba69a274b028d3a056d9173b66e53556dfaab
Ned Wolpert <ned.wolpert@gmail.com>
2024-12-28 11:01:37 -0700

    Added value objects for ids

---
+++ lib/src/main/java/com/codeheadsystems/tracking/manager/CounterManager.java
@@ -0,0 +1,10 @@
+package com.codeheadsystems.tracking.manager;
+
+import com.codeheadsystems.tracking.model.Counter;
+
+/**
+ * The interface Counter manager.
+ */
+public interface CounterManager extends SubjectComponentManager<Counter> {
+
+}
---
+++ lib/src/main/java/com/codeheadsystems/tracking/manager/NoteManager.java
@@ -0,0 +1,10 @@
+package com.codeheadsystems.tracking.manager;
+
+import com.codeheadsystems.tracking.model.Note;
+
+/**
+ * The interface Note manager.
+ */
+public interface NoteManager extends SubjectComponentManager<Note> {
+
+}
